9feabe09e4969ff867de92c3521f712cd619a735
Carl0sPineda <carlos7712p@gmail.com>
2024-03-12 19:09:59 -0600

    feat: Get an user by id

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -5,6 +5,6 @@ import { isAuth } from "../middlewares/verify.token";
 const router = express.Router();
 
 router.get("/users", isAuth, authController.getAllUsers);
-router.get("/user/:id", authController.getUserById);
+router.get("/users/:id", isAuth, authController.getUserById);
 
 export default router;
